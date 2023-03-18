afe76761bf02cfcd6d1e66eade017ece9724aa6a
marcelohdsantos <marcelodossantos732@yahoo.com.br>
2023-03-18 11:35:25 -0400

    requirements create and paginator

--- contacts/views.py
+++ contacts/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contact
 from django.http import Http404
+from django.core.paginator import Paginator
 
 
 def index(request):
     contacts = Contact.objects.all()
+    paginator = Paginator(contacts, 25)
+
+    page = request.GET.get('p')
+    contacts = paginator.get_page(page)
 
     return render(request, 'contacts/index.html', {
         'contacts': contacts
